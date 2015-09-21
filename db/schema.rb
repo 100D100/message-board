67ef4d5d8e212737d5502057fc34010107b5296a
a100d100 <addor0guileness@gmail.com>
2015-09-21 06:53:28 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914133026) do
+ActiveRecord::Schema.define(version: 20150918040050) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
